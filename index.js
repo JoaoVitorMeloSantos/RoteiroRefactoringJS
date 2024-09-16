f188299986b262fd39bf29c6a9d3eb081047cefc
João Vítor <joaovitor502013@gmail.com>
2024-09-15 22:45:04 -0300

    Commit 8 - Classe Repositorio

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { readFileSync } = require('fs');
 const ServicoCalculoFatura = require('./ServicoCalculoFatura');
+const Repositorio = require('./Repositorio');
 
-const calc = new ServicoCalculoFatura();
+
+const calc = new ServicoCalculoFatura(new Repositorio());
 
 function formatarMoeda(valor) {
   return new Intl.NumberFormat("pt-BR", {
@@ -11,17 +13,14 @@ function formatarMoeda(valor) {
   }).format(valor / 100);
 }
 
-function getPeca(pecas, apre) {
-  return pecas[apre.id];
-}
-
-function gerarFaturaStr(fatura, pecas, calc) {
+function gerarFaturaStr(fatura, calc) {
   let faturaStr = `Fatura ${fatura.cliente}\n`;
   for (let apre of fatura.apresentacoes) {
-    faturaStr += `  ${getPeca(pecas, apre).nome}: ${formatarMoeda(calc.calcularTotalApresentacao(pecas, apre))} (${apre.audiencia} assentos)\n`;
+    faturaStr += `  ${calc.repo.getPeca(apre).nome}: ${formatarMoeda(calc.calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
   }
-  faturaStr += `Valor total: ${formatarMoeda(calc.calcularTotalFatura(pecas, fatura.apresentacoes))}\n`;
-  faturaStr += `Créditos acumulados: ${calc.calcularTotalCreditos(pecas, fatura.apresentacoes)} \n`;
+  faturaStr += `Valor total: ${formatarMoeda(calc.calcularTotalFatura(fatura.apresentacoes))}\n`;
+  faturaStr += `Créditos acumulados: ${calc.calcularTotalCreditos(fatura.apresentacoes)} \n`;
+
   return faturaStr;
 }
 
@@ -38,8 +37,6 @@ function gerarFaturaHTML(fatura, pecas, calc) {
 */
 
 const faturas = JSON.parse(readFileSync('./faturas.json'));
-const pecas = JSON.parse(readFileSync('./pecas.json'));
 
-const faturaStr = gerarFaturaStr(faturas, pecas, calc);
+const faturaStr = gerarFaturaStr(faturas, calc);
 console.log(faturaStr);
-
